68508678642ecf9d198afe0398dcdb66a268119c
Oliva <irakizaoliva09@gmail.com>
2023-07-21 12:01:47 -0700

    create add speaker form

--- speaker/urls.py
+++ speaker/urls.py
@@ -3,5 +3,6 @@ from speaker import views
 
 urlpatterns = [
     path('all-speakers/', views.all_speakers, name=''),
+    path('add-speaker/', views.add_speaker_form_view, name=''),
     path('<str:name>/', views.speaker_detail, name='')
 ]
--- speaker/views.py
+++ speaker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from speaker.models import Speaker
+from .forms import SpeakerForm
+
 # Create your views here.
 def all_speakers(request):
     speaker = Speaker.objects.all()
@@ -8,3 +10,11 @@ def all_speakers(request):
 def speaker_detail( request, name):
         speaker = Speaker.objects.get(name=name)
         return render(request, 'speaker/speakerdetail.html', {'speaker': speaker})
+
+def add_speaker_form_view(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = SpeakerForm()
+        context={"form": form }
+    return render(request, 'speaker/addSpeakerForm.html', context)
