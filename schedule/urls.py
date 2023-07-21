2c9ab9cc860821cb6125d77e3c4b200c16044e84
Philo <cyubahirophilomene@gmail.com>
2023-07-21 11:11:16 -0700

    create add schedule form

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from schedule import views
 
 urlpatterns = [
-    path('all-schedules/', views.schedule,name='')
+    path('all-schedules/', views.schedule,name=''),
+    path('add-schedule/', views.schedule_form),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from schedule.models import Schedule
+from schedule.forms import ScheduleForm
 # Create your views here.
 def schedule(request):
     schedules = Schedule.objects.all()
     return render(request, 'schedule/schedule.html', {'schedules': schedules})
+
+def schedule_form(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = ScheduleForm()
+        context={"form": form }
+    return render(request, 'schedule/addscheduleform.html', context)
 
 
